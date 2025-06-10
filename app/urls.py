d6fc0586e0931128569c8bad532e315433e955e9
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-06-10 10:02:43 +0900

    로그인 및 회원가입 기능 추가: URL 경로 설정, 뷰 함수 구현, 정적 파일 경로 설정

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 from django.urls import path
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login_process(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    user = authenticate(request, email=email, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('index')
+    else:
+        return redirect('login')
+    
+def signup_process(request):
+    email = request.POST.get('email')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = User.objects.create_user(email=email, username=username, password=password)
+    user.save()
+
+    return redirect('login')
