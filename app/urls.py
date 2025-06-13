fba408d67794793eadba1301d5fc8ad1f2569e78
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-06-13 12:25:05 +0900

    방 선택 URL 및 템플릿 삭제: select_room 경로 제거 및 관련 HTML 파일 삭제

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('room/create/', views.create_room, name='create_room'),
-    path('room/select/', views.select_room, name='select_room'),
     path('room/<int:room_id>/', views.room_detail, name='room_detail'),
 ]
