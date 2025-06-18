7bb3c534b427de91147cc7f1b4fd6dc031140144
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-06-19 08:53:28 +0900

    프로젝트 끝ㅌㅌㅌ

--- user/urls.py
+++ user/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # ← 이게 있어야 함
 ]
